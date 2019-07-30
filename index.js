8b04d5d5ec57f2bcd7d07fd35b2f9fa20c634fb5
samagragupta <samagraguptajpr@gmail.com>
2019-07-30 13:45:24 +0530

    active link in header

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-          window.location.hash = hash;
-        });
-      }
-    });
+$(document).ready(function () {
+  $("a").on('click', function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+        window.location.hash = hash;
+      });
+    }
   });
-  
+});
+
+$(document).ready(function(){
+  $('.nav-link').click(function(){
+    $('.nav-link').removeClass("active");
+    $(this).addClass("active");
+});
+});
