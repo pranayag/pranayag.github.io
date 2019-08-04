c16dbdcd50dba2e933ebae23c4dcf949bf030f9e
samagragupta <samagraguptajpr@gmail.com>
2019-08-04 17:44:42 +0530

    Added scroll to top

--- index.js
+++ index.js
@@ -12,9 +12,24 @@ $(document).ready(function () {
   });
 });
 
-$(document).ready(function(){
-  $('.nav-link').click(function(){
+$(document).ready(function () {
+  $('.nav-link').click(function () {
     $('.nav-link').removeClass("active");
     $(this).addClass("active");
+  });
 });
-});
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
